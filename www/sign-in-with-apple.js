c782d9168c87b7bac21149998451e9ead2a4c39d
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-02-23 10:50:40 +0400

    Update sign-in-with-apple.js

--- www/sign-in-with-apple.js
+++ www/sign-in-with-apple.js
@@ -1,6 +1,10 @@
 var exec = require('cordova/exec');
 
 module.exports = {
+  Scope: {
+    FullName: 0,
+    Email: 1
+  },
   request: function(options) {
     return new Promise(function(resolve, reject) {
       cordova.exec(
