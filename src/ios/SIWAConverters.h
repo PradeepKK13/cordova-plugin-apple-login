b87131e83ba777af7c66fe93ecbcafcaa63ad5a5
Srini-max <54628670+Srini-max@users.noreply.github.com>
2020-02-23 13:15:59 +0400

    Delete SIWAConverters.h

--- src/ios/SIWAConverters.h
+++
@@ -1,8 +0,0 @@
-@interface SIWAConverters: NSObject
-
-+ (NSArray<ASAuthorizationScope> *)convertScopes: (NSArray<NSNumber *> *)scopes;
-+ (ASAuthorizationOpenIDOperation)convertOperation: (NSNumber *)operation;
-
-+ (NSDictionary *)convertCredential: (ASAuthorizationAppleIDCredential *)credential;
-
-@end
